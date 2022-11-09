5976c8d72df710bc8d7d022582cbcf1631e0430d
Chris <moltencore95@hotmail.de>
2022-11-09 21:14:59 +0100

    react crashcourse

---
+++ Counter.jsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+
+
+
+function Counter() {
+ const [cart, setCart] = useState ({ 
+    item: "apple",
+     quantity: 0,
+});
+
+function addApple () {
+    setCart(prevCart => ({
+        ...prevCart,
+        quantity: prevCart.quantity + 1, 
+    }))
+}
+
+
+function removeApple () {
+    setCart(prevCart => ({
+        ...prevCart,
+        quantity: prevCart.quantity - 1,
+        
+    }))
+}
+
+
+    return(
+        <div>
+            <button onClick={removeApple}>-</button>
+            {cart.quantity}
+            {cart.item}
+            <button onClick={addApple}>+</button>
+            </div>
+    )
+}
+
+export default Counter
